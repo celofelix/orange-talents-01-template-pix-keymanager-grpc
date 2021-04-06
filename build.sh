146d95a02c78947ef126c9d31c574f9bc30a6939
Marcelo Felix <celofelix93@gmail.com>
2021-04-06 18:24:25 -0300

    Alterado build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -vf orange-talents-01-template-pix-keymanager-grpc.zip
 zip -r orange-talents-01-template-pix-keymanager-grpc.zip *
-aws s3 orange-talents-01-template-pix-keymanager-grpc.zip s3://pix-grpc/
+aws s3 cp orange-talents-01-template-pix-keymanager-grpc.zip s3://pix-grpc/
