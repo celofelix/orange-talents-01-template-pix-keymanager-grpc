4696628508b34c193c7250e9d24f352f7b0a0899
Marcelo Felix <celofelix93@gmail.com>
2021-04-06 18:14:30 -0300

    Alterado nome do zip no build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-rm -vf pix-keymanager-grpc.zip
-zip -r pix-keymanager-grpc.zip *
-aws s3 pix-keymanager-grpc.zip s3://pix-grpc/
+rm -vf orange-talents-01-template-pix-keymanager-grpc.zip
+zip -r orange-talents-01-template-pix-keymanager-grpc.zip *
+aws s3 orange-talents-01-template-pix-keymanager-grpc.zip s3://pix-grpc/
